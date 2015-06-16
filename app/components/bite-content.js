30c1ab525abce38c15041fabf4a2e11c58209336
Abhi Pillai <abhi.pillai1@gmail.com>
2015-06-15 21:54:10 -0500

    Remove bites when you click complete

--- app/components/bite-content.js
+++ app/components/bite-content.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+             complete: function(bite) {
+                         bite.set('isComplete', true);
+                       }
+           }
 });
--- app/models/bite.js
+++ app/models/bite.js
@@ -2,23 +2,27 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  videoId: DS.attr('string')
+  videoId: DS.attr('string'),
+  isComplete: DS.attr('boolean')
 }).reopenClass({
   FIXTURES: [
     {
       id: 1,
       title: "Complete Ember.js Tutorial",
-      videoId: "video1"
+      videoId: "video1",
+      isComplete: false
     },
     {
       id: 2,
       title: "Checkout some more ember stuff",
-      videoId: "video2"
+      videoId: "video2",
+      isComplete: true
     },
     {
       id: 3,
       title: "Solve world hunger (with Ember)",
-      videoId: "video3"
+      videoId: "video3",
+      isComplete: false
     }
   ]
 });
